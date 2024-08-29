887a7e946bd6172f73e1fb9ee65fd99df400ef01
albornet <alban.bornet@gmail.com>
2024-08-29 13:09:39 +0200

    removed unecessary sudos

--- send_sif_file.sh
+++ send_sif_file.sh
@@ -6,33 +6,33 @@ SERVER_ADDRESS="borneta@login2.baobab.hpc.unige.ch"
 SIF_FOLDER_LOCATION="/home/users/b/borneta/sif"
 
 # Build the Docker image
-sudo docker build -t $IMAGE_NAME-image:latest .
+docker build -t $IMAGE_NAME-image:latest .
 if [ $? -ne 0 ]; then
     echo "Docker build failed. Exiting."
     exit 1
 fi
 
 # Save the Docker image to a tar file
-sudo docker save $IMAGE_NAME-image:latest > $IMAGE_NAME-image.tar
+docker save $IMAGE_NAME-image:latest > $IMAGE_NAME-image.tar
 if [ $? -ne 0 ]; then
     echo "Docker save failed. Exiting."
     exit 1
 fi
 
-# Prompt the user to clean docker cache (my computer disk is small...)
-read -p "Do you want to clean up all unused Docker resources? [y/N]: " -r
-echo  # new line
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-    sudo docker image prune -f
-    sudo docker container prune -f
-    sudo docker builder prune -f
-    sudo docker system prune -f
-else
-    echo "Skipping docker cache cleaning"
-fi
+# # Prompt the user to clean docker cache (my computer disk is small...)
+# read -p "Do you want to clean up all unused Docker resources? [y/N]: " -r
+# echo  # new line
+# if [[ $REPLY =~ ^[Yy]$ ]]; then
+#     docker image prune -f
+#     docker container prune -f
+#     docker builder prune -f
+#     docker system prune -f
+# else
+#     echo "Skipping docker cache cleaning"
+# fi
 
 # Convert the Docker tar file to a Singularity (Apptainer) SIF image
-sudo apptainer build $IMAGE_NAME-image.sif docker-archive://$IMAGE_NAME-image.tar
+apptainer build $IMAGE_NAME-image.sif docker-archive://$IMAGE_NAME-image.tar
 if [ $? -ne 0 ]; then
     echo "Apptainer build failed. Exiting."
     exit 1
@@ -46,7 +46,7 @@ if [ $? -ne 0 ]; then
 fi
 
 # Remove the Docker image and tar file to clean up space
-sudo docker rmi $IMAGE_NAME-image:latest
+docker rmi $IMAGE_NAME-image:latest
 rm $IMAGE_NAME-image.tar
 rm $IMAGE_NAME-image.sif
 

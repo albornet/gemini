0420d6b8477d3828fa9cc65b780e11b0c432f0b2
albornet <alban.bornet@gmail.com>
2025-09-19 16:21:41 +0200

    corrected experiment 1 bug

--- experiments/experiment_1.sh
+++ experiments/experiment_1.sh
@@ -3,20 +3,20 @@
 MODEL_PATHS=(
     "unsloth/Qwen3-0.6B-GGUF"
     "unsloth/Qwen3-1.7B-GGUF"
-    # "unsloth/Qwen3-4B-GGUF"
-    # "unsloth/Qwen3-8B-GGUF"
-    # "unsloth/Qwen3-14B-GGUF"
-    # "unsloth/Qwen3-32B-GGUF"
+    "unsloth/Qwen3-4B-GGUF"
+    "unsloth/Qwen3-8B-GGUF"
+    "unsloth/Qwen3-14B-GGUF"
+    "unsloth/Qwen3-32B-GGUF"
 )
 
-QUANT_MODES=(
+QUANT_SCHEMES=(
     "IQ1_M"
     "Q2_K_XL"
-    # "Q3_K_XL"
-    # "Q4_K_XL"
-    # "Q5_K_XL"
-    # "Q6_K_XL"
-    # "Q8_K_XL"
+    "Q3_K_XL"
+    "Q4_K_XL"
+    "Q5_K_XL"
+    "Q6_K_XL"
+    "Q8_K_XL"
 )
 
 declare -A GPU_REQUIREMENTS
@@ -24,8 +24,10 @@ GPU_REQUIREMENTS["unsloth/Qwen3-14B-GGUF:Q8_K_XL"]="2"
 GPU_REQUIREMENTS["unsloth/Qwen3-32B-GGUF:Q4_K_XL"]="2"
 GPU_REQUIREMENTS["unsloth/Qwen3-32B-GGUF:Q5_K_XL"]="2"
 GPU_REQUIREMENTS["unsloth/Qwen3-32B-GGUF:Q6_K_XL"]="2"
-GPU_REQUIREMENTS["unsloth/Qwen3-32B-GGUF:Q8_K_XL"]="2"
+GPU_REQUIREMENTS["unsloth/Qwen3-32B-GGUF:Q8_K_XL"]="3"
 DEFAULT_NUM_GPUS=1
+
+TIME_TO_RUN="0-01:30:00"
 
 CONFIG_FILE="./configs/model_config.yaml"
 if [ ! -f "$CONFIG_FILE" ]; then
@@ -35,14 +37,14 @@ fi
 
 # Iterate over each model and each quantization mode
 for MODEL_PATH in "${MODEL_PATHS[@]}"; do
-    for QUANT_MODE in "${QUANT_MODES[@]}"; do
+    for QUANT_SCHEME in "${QUANT_SCHEMES[@]}"; do
         echo "--------------------------------------------------------"
         echo "   Preparing benchmark for:"
         echo "   Model: $MODEL_PATH"
-        echo "   Quantization: $QUANT_MODE"
+        echo "   Quantization: $QUANT_SCHEME"
 
         # Select the number of GPU to use for that combination
-        KEY="${MODEL_PATH}:${QUANT_MODE}"
+        KEY="${MODEL_PATH}:${QUANT_SCHEME}"
         NUM_GPUS=$DEFAULT_NUM_GPUS
         if [[ -v "GPU_REQUIREMENTS[$KEY]" ]]; then
             NUM_GPUS=${GPU_REQUIREMENTS[$KEY]}
@@ -56,15 +58,15 @@ for MODEL_PATH in "${MODEL_PATHS[@]}"; do
         # The '-i' flag modifies the file in-place.
         # We use '|' as the delimiter for 's' command to avoid conflicts with '/' in the MODEL_PATH.
         sed -i "s|^model_path:.*|model_path: $MODEL_PATH|" "$CONFIG_FILE"
-        sed -i "s|^quant_mode:.*|quant_mode: $QUANT_MODE|" "$CONFIG_FILE"
+        sed -i "s|^quant_scheme:.*|quant_scheme: $QUANT_SCHEME|" "$CONFIG_FILE"
         sed -i "s|^inference_backend:.*|inference_backend: llama-cpp|" "$CONFIG_FILE"
         echo "Updated '$CONFIG_FILE'."
 
         # Run the benchmark script
         echo "Running benchmark script..."
-        ./scripts/run_benchmark.sh -g "$NUM_GPUS"
-        echo "Benchmark job sent for $MODEL_PATH ($QUANT_MODE)."
-        echo "" # Add a newline for better readability
+        ./scripts/run_benchmark.sh -g "$NUM_GPUS" -t $TIME_TO_RUN
+        echo "Benchmark job sent for $MODEL_PATH ($QUANT_SCHEME)."
+        echo ""
 
         # Wait for 1 second to prevent a race condition
         sleep 1
